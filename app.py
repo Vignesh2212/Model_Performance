51f55968fd9718ec1048c21021043b1285973876
Vignesh2212 <subramanian.sekar22@gmail.com>
2022-05-31 14:58:56 +0530

    Added a page - define time windows

--- app.py
+++ app.py
@@ -3,23 +3,24 @@ import streamlit as st
 import numpy as np
 from PIL import  Image
 
-# Custom imports 
+# Custom imports
 from multipage import MultiPage
 from pages import data_upload, machine_learning, metadata, data_visualize, redundant # import your pages here
 
-# Create an instance of the app 
+# Create an instance of the app
 app = MultiPage()
 
 # Title of the main page
-display = Image.open('Logo.png')
-display = np.array(display)
+#display = Image.open('Logo.png')
+#display = np.array(display)
 # st.image(display, width = 400)
 # st.title("Data Storyteller Application")
-col1, col2 = st.beta_columns(2)
-col1.image(display, width = 400)
-col2.title("Data Storyteller Application")
+#col1, col2 = st.beta_columns(2)
+#col1.image(display, width = 400)
+#col2.title("Data Storyteller Application")
 
 # Add all your application here
+app.add_page("Define Time Window", define_windows.app)
 app.add_page("Upload Data", data_upload.app)
 app.add_page("Change Metadata", metadata.app)
 app.add_page("Machine Learning", machine_learning.app)
